3806af4345d9fcdb55f3f08618b10c6252bfc528
Luckysinghchauhan <120488960+kookie001@users.noreply.github.com>
2024-10-29 18:43:30 +0530

    config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_5t6L1p3Mx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1bdkq1ulglaggekndhsm7vv8ri', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
